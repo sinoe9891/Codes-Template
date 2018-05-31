83da43f044a457cb2e37489f65978ecbe390344d
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2018-05-31 09:37:30 -0600

    Movimiento

--- Index.php
+++ Index.php
@@ -8,7 +8,7 @@
   <title>Code Template - Web Developer</title>
   <link rel="icon" href="./includes/avatar.png" type="image/x-icon" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="./includes/css" rel="stylesheet">
+  <link href="./includes/css/css.css" rel="stylesheet">
  <!-- Optimización SEO -->
 <meta name="description" content="Sinoé is a web developer based in Tegucigalpa, Honduras. great fast web design.">
 <link rel="canonical" href="https://www.creativehonduras.com/codes-template">
@@ -19,15 +19,15 @@
 <meta property="og:url" content="https://www.creativehonduras.com/codes-template">
 <meta property="og:site_name" content="Codes Template - Web Developer">
 <meta property="og:image" content="https://www.creativehonduras.com/codes-template/includes/content/uploads/2018/05/portfolio-copy.jpg">
-<!-- <meta property="og:image:secure_url" content="https://www.creativehonduras.com/codes-template/includes/content/uploads/2018/05/portfolio-copy.jpg">
+<meta property="og:image:secure_url" content="https://www.creativehonduras.com/codes-template/includes/content/uploads/2018/05/portfolio-copy.jpg">
 <meta property="og:image:width" content="1200">
-<meta property="og:image:height" content="700"> -->
-<!-- <meta name="twitter:card" content="summary_large_image">
+<meta property="og:image:height" content="700">
+<meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:description" content="Sinoé is a web developer based in Tegucigalpa, Honduras. great fast web design.">
 <meta name="twitter:title" content="Code Template - Web Developer">
 <meta name="twitter:site" content="@richardcodes">
 <meta name="twitter:image" content="./includes/content/uploads/2018/05/portfolio-copy.jpg">
-<meta name="twitter:creator" content="@richardcodes"> -->
+<meta name="twitter:creator" content="@richardcodes">
 <!-- / Yoast SEO plugin. -->
 <script src="./includes/js/wp-emoji-release.min.js" type="text/javascript" defer=""></script>
 		<style type="text/css">
@@ -44,7 +44,7 @@ img.emoji {
 	padding: 0 !important;
 }
 </style>
-<link rel="stylesheet" id="richardcodes-style-css" href="./includes/style.css" type="text/css" media="all">
+<link rel="stylesheet" id="richardcodes-style-css" href="./includes/css/style.css" type="text/css" media="all">
 <script type="text/javascript" src="./includes/js/jquery.js"></script>
 <script type="text/javascript" src="./includes/js/jquery-migrate.min.js"></script>
 <link rel="shortlink" href="https://www.creativehonduras.com/">
